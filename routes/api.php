3d9b06b14595aa8f2fe22dc9cd5afcdd3ff1ace0
vietdm <vietdau33@gmail.com>
2023-10-05 01:08:14 +0700

    Add version to API

--- routes/api.php
+++ routes/api.php
@@ -9,25 +9,27 @@ Route::get('healthy', function () {
     return Response::success('OK');
 });
 
-Route::prefix('board')->group(function() {
-    Route::get('all', [BoardController::class, 'getBoards']);
-    Route::get('{uuid}', [BoardController::class, 'getBoard']);
-
-    Route::post('create', [BoardController::class, 'createBoard']);
-
-    Route::put('{uuid}', [BoardController::class, 'updateBoard']);
-
-    Route::delete('{uuid}', [BoardController::class, 'removeBoard']);
-});
-
-Route::prefix('task')->group(function() {
-    Route::get('all', [TaskController::class, 'getTasks']);
-    Route::get('{uuid}', [TaskController::class, 'getTask']);
-
-    Route::post('create', [TaskController::class, 'addTask']);
-
-    Route::put('{uuid}', [TaskController::class, 'updateTask']);
-    Route::put('{uuid}/move-to/{board-uuid}', [TaskController::class, 'moveBoardOfTask']);
-
-    Route::delete('{uuid}', [TaskController::class, 'removeTask']);
+Route::prefix('v1')->group(function() {
+    Route::prefix('board')->group(function() {
+        Route::get('all', [BoardController::class, 'getBoards']);
+        Route::get('{uuid}', [BoardController::class, 'getBoard']);
+    
+        Route::post('create', [BoardController::class, 'createBoard']);
+    
+        Route::put('{uuid}', [BoardController::class, 'updateBoard']);
+    
+        Route::delete('{uuid}', [BoardController::class, 'removeBoard']);
+    });
+    
+    Route::prefix('task')->group(function() {
+        Route::get('all', [TaskController::class, 'getTasks']);
+        Route::get('{uuid}', [TaskController::class, 'getTask']);
+    
+        Route::post('create', [TaskController::class, 'addTask']);
+    
+        Route::put('{uuid}', [TaskController::class, 'updateTask']);
+        Route::put('{uuid}/move-to/{board-uuid}', [TaskController::class, 'moveBoardOfTask']);
+    
+        Route::delete('{uuid}', [TaskController::class, 'removeTask']);
+    });
 });
